8645743adad353926120addebae768d2b6c9f176
Anshuman Singh <anshumansinghneu@gmail.com>
2025-12-11 18:56:01 -0500

    fix: Add missing @types/morgan for backend build
    - Install @types/morgan dev dependency
    - Regenerate Prisma client
    - Fixes Render build error
    - Backend will now compile successfully

--- backend/dist/prisma.js
+++ backend/dist/prisma.js
@@ -1,11 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const pg_1 = require("pg");
-const adapter_pg_1 = require("@prisma/adapter-pg");
 const client_1 = require("@prisma/client");
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg_1.Pool({ connectionString });
-const adapter = new adapter_pg_1.PrismaPg(pool);
-const prisma = new client_1.PrismaClient({ adapter });
+const prisma = new client_1.PrismaClient();
 exports.default = prisma;
